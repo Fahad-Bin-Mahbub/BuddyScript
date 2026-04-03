86accdb19ac1ad3705c1c530e4d28981fd33aa47
Fahad Bin Mahbub <fahadbm3234@gmail.com>
2026-04-04 02:33:02 +0600

    Global CSS changes

--- next.config.ts
+++ next.config.ts
@@ -1,20 +1,28 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    // Avoid workspace root inference panics in Next 16+ using PostCSS 
-    // @ts-ignore
-    root: __dirname,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        pathname: "/**",
-      },
-    ],
-  },
+	turbopack: {
+		// Avoid workspace root inference panics in Next 16+ using PostCSS
+		// @ts-ignore
+		root: __dirname,
+	},
+	images: {
+    unoptimized:true,
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "res.cloudinary.com",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "api.dicebear.com",
+				port: "",
+				pathname: "/7.x/**",
+			},
+		],
+	},
+
 };
 
 export default nextConfig;
